90b5dd8bac3828a36146cb025c495e7610a9c4b4
Johann S <johann@MacBook-Air-von-Johann.local>
2026-04-20 13:42:11 +0200

    Rename Product to Price in shop embed

--- utils/orderUtils.js
+++ utils/orderUtils.js
@@ -25,7 +25,7 @@ function buildShopEmbed(item) {
     .setColor(0x2b2d31)
     .setTitle(item.title)
     .addFields(
-      { name: 'Product', value: item.price }
+      { name: 'Price', value: item.price }
     );
 
   if (item.imageUrl) embed.setImage(item.imageUrl);
